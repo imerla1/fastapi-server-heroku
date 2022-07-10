7566ebfa5941c5476ea68465492211cafbb9eb8d
imerla1 <giorgi1048@yahoo.com>
2022-07-10 13:06:02 +0400

    redircet from index to swagger page

--- main.py
+++ main.py
@@ -11,13 +11,8 @@ import random
 import string
 import time
 import os
+from fastapi.responses import RedirectResponse
 
-
-class Settings(BaseSettings):
-    # ... The rest of our FastAPI settings
-
-    BASE_URL = "http://localhost:8000"
-    USE_NGROK = os.environ.get("USE_NGROK", "False") == "True"
 
 
 app = FastAPI(
@@ -130,6 +125,13 @@ async def get_current_active_user(current_user: UserBase = Depends(get_current_u
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
+@app.get("/")
+async def root():
+    # redirect from index page to swagger docs
+    return RedirectResponse(
+        "/docs"
+    )
+
 
 @app.get("/get/db")
 async def get_db():
@@ -195,30 +197,3 @@ async def log_requests(request: Request, call_next):
     return response
 
 
-def init_webhooks(base_url):
-    # Update inbound traffic via APIs to use the public-facing ngrok URL
-    pass
-
-
-# Initialize the FastAPI app for a simple web server
-app = FastAPI()
-settings = Settings()
-if settings.USE_NGROK:
-    # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
-    from pyngrok import ngrok
-
-    # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
-    # when starting the server
-    port = sys.argv[sys.argv.index(
-        "--port") + 1] if "--port" in sys.argv else 8000
-
-    # Open a ngrok tunnel to the dev server
-    public_url = ngrok.connect(port).public_url
-    logger.info(
-        "ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
-
-    # Update any base URLs or webhooks to use the public ngrok URL
-    settings.BASE_URL = public_url
-    init_webhooks(public_url)
-
-# ... Initialize routers and the rest of our app
